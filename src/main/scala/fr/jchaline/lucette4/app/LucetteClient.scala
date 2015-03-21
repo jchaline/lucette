15d4037d5b1988a5d49db05348e7251b53092aca
jchaline <chaline.jeremy@gmail.com>
2015-03-21 11:33:15 +0100

    Préparation pour serveur et client

--- src/main/scala/fr/jchaline/lucette4/app/LucetteClient.scala
+++ src/main/scala/fr/jchaline/lucette4/app/LucetteClient.scala
@@ -1,8 +1,17 @@
 package fr.jchaline.lucette4.app
 
 /**
- * Created by jeremy on 13/03/15.
+ * Executable permettant de créer un client,
+ * Les clients se connectent à un serveur pour créer des parties et jouer contre d'autres adversaires
  */
 class LucetteClient {
+
+  /**
+   * Launcher du client
+   * @param args arguments
+   */
+  def main( args:Array[String] ): Unit ={
+
+  }
 
 }
---
+++ src/main/scala/fr/jchaline/lucette4/app/LucetteServer.scala
@@ -0,0 +1,16 @@
+package fr.jchaline.lucette4.app
+
+/**
+ * Executable permettant de créer le serveur,
+ * Les clients doivent s'y connecter pour créer des parties et jouer contre d'autres adversaires
+ */
+class LucetteServer {
+
+  /**
+   * Launcher du serveur de jeu
+   * @param args arguments
+   */
+  def main( args:Array[String] ): Unit ={
+
+  }
+}
