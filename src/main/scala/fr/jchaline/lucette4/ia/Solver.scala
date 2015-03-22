647f45bea887158b7b8947be1a055d7b28b751f3
jchaline <chaline.jeremy@gmail.com>
2015-03-22 21:13:42 +0100

    Ajout javadoc

--- src/main/scala/fr/jchaline/lucette4/ia/Solver.scala
+++ src/main/scala/fr/jchaline/lucette4/ia/Solver.scala
@@ -2,8 +2,19 @@ package fr.jchaline.lucette4.ia
 
 import fr.jchaline.lucette4.game.{Coord, DameBoard}
 
+/**
+ * Interface pour les différents algorithmes de résolution du meilleur mouvement
+ */
 trait Solver {
 
+  /**
+   * Point d'entrée de la recherche du meilleur mouvement
+   * @param node plateau à évaluer
+   * @param depth profondeur, permet de limiter la recherche en temps/resources
+   * @param heuristic fonction d'évaluation d'un noeud
+   * @param childrens fonction permettant d'obtenir les noeuds suivants
+   * @return le meilleur mouvement présumé
+   */
   def bestMove(node:DameBoard, depth:Int,
                heuristic:(DameBoard) => Int,
                childrens:(DameBoard) => List[DameBoard]):Coord
